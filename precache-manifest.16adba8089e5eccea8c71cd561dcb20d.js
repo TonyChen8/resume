fafce145dc9479d7dfaa18b9cb9dfc59a12bd71a
chen ming <chenming8302@gmail.com>
2020-03-04 10:24:34 +1000

    Updates

---
+++ precache-manifest.16adba8089e5eccea8c71cd561dcb20d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1529abd1dd442777407",
+    "url": "/resume/static/js/app.4e1faa9e.chunk.js"
+  },
+  {
+    "revision": "510d00d29cbfe5a33d40",
+    "url": "/resume/static/js/runtime~app.0979135c.js"
+  },
+  {
+    "revision": "f320b6197e81afeab5ad",
+    "url": "/resume/static/js/2.3d3d2b5d.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/resume/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/resume/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/resume/expo-service-worker.js"
+  },
+  {
+    "revision": "432600a021f136cf936c6a906019701b",
+    "url": "/resume/manifest.json"
+  },
+  {
+    "revision": "e741ccf1172af3c5c3244dacdffd322c",
+    "url": "/resume/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "dcaeb12f332621f575c6538b03d13081",
+    "url": "/resume/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "e09cdd7b98510619be3683ad88278d5d",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "915d10feadb96d622b9fef559989838c",
+    "url": "/resume/static/js/runtime~app.0979135c.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/resume/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "e7ad757d5a4fc984e138ed8d81977b83",
+    "url": "/resume/static/js/app.4e1faa9e.chunk.js.gz"
+  },
+  {
+    "revision": "565470693cd424bc6d4c8bfd817701aa",
+    "url": "/resume/static/js/2.3d3d2b5d.chunk.js.gz"
+  }
+];
---
+++ precache-manifest.cdc77ab1a08126b48fa3edc2c90b8588.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1529abd1dd442777407",
+    "url": "/resume/static/js/app.4e1faa9e.chunk.js"
+  },
+  {
+    "revision": "510d00d29cbfe5a33d40",
+    "url": "/resume/static/js/runtime~app.0979135c.js"
+  },
+  {
+    "revision": "6842d188214d66c4f326",
+    "url": "/resume/static/js/2.a3a8913b.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/resume/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/resume/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/resume/expo-service-worker.js"
+  },
+  {
+    "revision": "432600a021f136cf936c6a906019701b",
+    "url": "/resume/manifest.json"
+  },
+  {
+    "revision": "e741ccf1172af3c5c3244dacdffd322c",
+    "url": "/resume/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "dcaeb12f332621f575c6538b03d13081",
+    "url": "/resume/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "19a84c85920f77ebace951788ec363f8",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "915d10feadb96d622b9fef559989838c",
+    "url": "/resume/static/js/runtime~app.0979135c.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/resume/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "e7ad757d5a4fc984e138ed8d81977b83",
+    "url": "/resume/static/js/app.4e1faa9e.chunk.js.gz"
+  },
+  {
+    "revision": "102e74ddff28ebcf4f44def5022c95fa",
+    "url": "/resume/static/js/2.a3a8913b.chunk.js.gz"
+  }
+];
